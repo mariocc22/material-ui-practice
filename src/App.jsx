ee8da52ca5a84602fd1b9f6e9b937e359843eac6
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-05-21 08:44:37 -0700

    adding the theme for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import "./App.css";
 
 // components
 import TourCard from "./components/TourCard.jsx";
+import SearchAppBar from "./components/AppBar";
 
 // material ui
 import Container from "@mui/material/Container";
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+
+// data
+import cities from "./data/dataCards.json";
 
 function App() {
   return (
     <>
-      <Container>
-        <Grid container spacing={1}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container
+        sx={{
+          marginY: "3rem",
+        }}
+      >
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+              key={city.id}
+            >
+              Top {city.name} tours
+            </Typography>
+            <Grid container spacing={1}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </>
   );
--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,64 +1,97 @@
+/* eslint-disable react/prop-types */
 // material ui
 import Paper from "@mui/material/Paper";
-import { Grid, Typography, Box, Rating } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Box,
+  Rating,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
 
-const TourCard = () => {
+// custom properties
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: "11px",
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: "9px",
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://images.unsplash.com/photo-1630051822408-b80dde2f5681?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          alt="nintendo"
-        />
-        <Box paddingX={1}>
-          <Typography component="h2" variant="subtitle1">
-            Immerse into the Falls
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt="tour image" />
+          <Box paddingX={1}>
+            <Typography component="h2" variant="subtitle1">
+              {tour.name}
+            </Typography>
+            <Box
               sx={{
-                width: "12.5px",
+                display: "flex",
+                alignItems: "center",
               }}
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
-            </Typography>
+            >
+              <AccessTime
+                sx={{
+                  width: "12.5px",
+                }}
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: "1rem",
+              }}
+            >
+              {" "}
+              <Rating
+                name="read-only"
+                value={tour.rating}
+                readOnly
+                precision={0.5}
+                size="small"
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                ({tour.numberOrReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: "1rem",
-            }}
-          >
-            {" "}
-            <Rating
-              name="read-only"
-              value={4.5}
-              readOnly
-              precision={0.5}
-              size="small"
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
