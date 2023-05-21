43763d7fb715568ae7214b348b558283255c32dc
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-05-21 07:22:57 -0700

    grid layout

---
+++ src/components/TourCard.jsx
@@ -0,0 +1,14 @@
+// material ui
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { Grid } from "@mui/material";
+
+const TourCard = () => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>TourCard</Paper>
+    </Grid>
+  );
+};
+
+export default TourCard;
